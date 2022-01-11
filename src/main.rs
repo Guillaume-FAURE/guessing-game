02dc3e5844b83ea2fd766e73143ea59193a7266d
Guillaume-FAURE <faureg0717@gmail.com>
2022-01-11 11:40:44 +0100

    initialisation of the repository

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main(){
+    println!("GUESS THE NUMBER :");
+
+    let secret_number = rand::thread_rng().gen_range(1..10001);
+    
+    //println!("The secret number is {}", secret_number);
+    
+    loop {
+        println!("\nInput your guess:");
+        
+        let mut guess=String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read the line! Try again!");
+        
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number");
+                continue;
+            }
+        };
+
+        println!("You guess {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("Good job, you guess the rigth number!!");
+                break
+            }
+        }
+        println!("Try a new guess :)");
+    }
+}
+    
